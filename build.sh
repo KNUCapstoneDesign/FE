1972b304755d91d6ddfd103ee56d6fc79fdde216
Ricky Song <gunmagician@naver.com>
2025-03-04 19:31:25 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+
+# 프로젝트의 루트로 이동
 cd ../
-mkdir output
+
+# output 디렉토리 생성
+mkdir -p output
+
+# FE 디렉토리의 모든 파일을 output 폴더로 복사
 cp -R ./FE/* ./output
-cp -R ./output ./FE/
+
+# output 디렉토리 전체를 FE 폴더로 다시 복사 (목적지에 맞게 수정)
+cp -R ./output/* ./FE/
